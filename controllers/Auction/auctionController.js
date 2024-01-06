75e6736c0f1f08bc167c2d46628fbe2c258b7895
rishishah1102 <rishi.shah545@gmail.com>
2024-01-06 15:13:29 +0530

    small change in auctionController

--- controllers/Auction/auctionController.js
+++ controllers/Auction/auctionController.js
@@ -3,49 +3,37 @@ const userModel = require("../../models/userModel");
 
 const auctionController = async (req, res) => {
   try {
-    const auctionData = await userModel.aggregate([
-      {
-        $match: { isPlaying: true },
-      },
-      {
-        $lookup: {
-          from: "players",
-          localField: "squad",
-          foreignField: "_id",
-          as: "squadDetails",
-        },
-      },
-      {
-        $project: {
-          username: 1,
-          ImgUrl: 1,
-          teamname: 1,
-          overseas: {
-            $sum: {
-              $cond: [
-                {
-                  $and: [
-                    { $ne: ["$squadDetails", []] },
-                    {
-                      $ne: [
-                        { $arrayElemAt: ["$squadDetails.country", 0] },
-                        "India",
-                      ],
-                    },
-                  ],
-                },
-                1,
-                0,
-              ],
-            },
-          },
-          purseUsed: {
-            $ifNull: [{ $sum: "$squadDetails.sellingPrice" }, 0],
-          },
-          boughtPlayers: { $size: "$squad" }, // New field
-        },
-      },
-    ]);
+    const findUsers = await userModel.find({ isPlaying: true }).lean();
+
+    let auctionData = await Promise.all(
+      findUsers.map(async (user) => {
+        let boughtPlayers = 0;
+        let overseas = 0;
+        let purseUsed = 0;
+
+        if (user.squad.length > 0) {
+          const squadPlayerIds = user.squad.map((playerId) => playerId.toString());
+          const squadPlayers = await playerModel.find({ _id: { $in: squadPlayerIds } }).lean();
+
+          squadPlayers.forEach((foundPlayer) => {
+            if (foundPlayer.country !== "India") {
+              overseas += 1;
+            }
+            boughtPlayers += 1;
+            purseUsed += foundPlayer.sellingPrice;
+          });
+        }
+
+        return {
+          username: user.username,
+          teamname: user.teamname,
+          ImgUrl: user.ImgUrl,
+          purseUsed,
+          overseas,
+          boughtPlayers,
+        };
+      })
+    );
 
     res.status(200).send({
       success: true,
